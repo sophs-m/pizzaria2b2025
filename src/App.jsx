d65c71ddf37e312615a29cb857eabce917255ed7
sophs-m <rm94485@estudante.fieb.edu.br>
2025-04-07 07:59:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
 import { useState } from "react"
-
+ 
 function App(){
+ 
+  
+ 
+  const Formulario = (props) => {
+ 
+    const [nome, setNome] = useState('Ronaldo')
+ 
+    return (
+<div>
+<h3>{props.titulo}</h3>
+<input className="nome"
 
-  const [nome, setNome] = useState('Ronaldo')
-
-  return (
-    <div>
-
-      <h3>Pizzaria 2B</h3>
-
-
-      <input className="nome"
       placeholder="Digite seu nome..."
+
       onChange={(e)=>{setNome(e.target.value)}}
 
       name="nome"
 
       type="text" />
-      <button className="botão" onClick={()=>alert(nome)}>
-      Clique Aqui 
+<button className="botao" onClick={
 
-      </button>
+        () => {
 
+          if(nome.length > 0)
+
+          alert(nome)
+
+        else
+
+          alert(props.mensagem)
+
+        }
+
+     }>
+ 
+        CLIQUE AQUI
+</button>
+</div>
+
+    )
+
+  }
+ 
+  return (
+<div>
+<h3>Pizzaria 2B</h3>
+<Formulario titulo = "Nome" mensagem = "Insira seu nome"/>
+<Formulario titulo = "E-mail" mensagem = "Insira seu E-mail"/>
+<Formulario titulo = "CPF" mensagem = "Insira seu CPF"/>
+<Formulario/>
+ 
     </div>
-  )
-}
 
+  )
+
+}
+ 
 export default App
+ 
